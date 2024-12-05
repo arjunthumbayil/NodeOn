9bac583488252b6936d46a29924fd3e380763828
Arjun Thumbayil <arjunthumbayil@gmail.com>
2024-12-05 16:20:58 +0530

    2nd commit

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res)=>{
-    res.send("Hello Arjun!");
+app.use("/test/2",(req, res)=>{
+    res.send("Hello Test 2!!!!!");
 })
 
 app.use("/test",(req, res)=>{
     res.send("Hello Test!!!!!");
 })
-
 app.use("/hello",(req, res)=>{
     res.send("Hello hello hello maan!!");
 })
 
+app.use((req, res)=>{
+    res.send("Hello Arjun!");
+})
 app.listen(3005, () => {
     console.log("Server is sucessfully running on port 3005....");
 }); 
