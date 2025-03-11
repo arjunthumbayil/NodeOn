0e3afd4cb5002ea026c4e2312ae73b820dc14c2e
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-11 20:32:41 +0530

    synced with Postman

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('You are right!');
+app.use('/hello/2', (req, res) => {
+  res.send('Abracadabra!');
+});
+
+app.use('/hello', (req, res) => {
+  res.send('hello hello hello!');
+});
+app.use('/test', (req, res) => {
+  res.send('Hello from the server, yeah right!');
 });
 
 app.listen(3005, () => {
   console.log('Server is sucessfully running on port 3005....');
 });
-
-//testing
