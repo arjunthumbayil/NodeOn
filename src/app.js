085c248fed3cd23b24365cbb9575e532b10eb9f2
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-11 22:49:34 +0530

    Post Added

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello/2', (req, res) => {
-  res.send('Abracadabra!');
+// this will only handle  GET call to API calls to /test
+app.get('/user', (req, res) => {
+  res.send({ firstName: 'John', lastName: 'Doe' });
 });
 
-app.use('/hello', (req, res) => {
-  res.send('hello hello hello!');
+app.post('/user', (req, res) => {
+  console.log('Save data to the DB.');
+  res.send('Data saved to DB successfully!');
 });
+
+// this will match all the HTTP method API calls to /test
 app.use('/test', (req, res) => {
   res.send('Hello from the server, yeah right!');
 });
