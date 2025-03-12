ef99535ab899526815d8ecea4f68a236d56f23af
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-12 23:42:50 +0530

    Starting with Episode-05 | Middlewares & Error Handlers

--- src/app.js
+++ src/app.js
@@ -2,26 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// this will only handle  GET call to API calls to /test
-app.get('/user', (req, res) => {
-  res.send({ firstName: 'John', lastName: 'Doe' });
-});
-
-app.post('/user', (req, res) => {
-  console.log('Save data to the DB.');
-  res.send('Data saved to DB successfully!');
-});
-
-app.delete('/user', (req, res) => {
-  console.log('Delete data from DB.');
-  res.send('Data deleted from DB successfully!');
-});
-
-// this will match all the HTTP method API calls to /test
-app.use('/test', (req, res) => {
-  res.send('Hello from the server, yeah right!');
-});
-
-app.listen(3005, () => {
+app.listen(7777, () => {
   console.log('Server is sucessfully running on port 3005....');
 });
