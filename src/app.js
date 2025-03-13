28b14e2d6626034b16c8217dab29c5d905207437
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-13 14:10:10 +0530

    added two route handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(
+  '/user',
+  (req, res) => {
+    console.log('Handling the route user1');
+    res.send('Response!');
+  },
+  (req, res) => {
+    console.log('Handling the route user2');
+    res.send('2nd Response!');
+  }
+);
+
 app.listen(7777, () => {
   console.log('Server is sucessfully running on port 3005....');
 });
