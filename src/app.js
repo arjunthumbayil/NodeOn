120940de6e56482df9efe42c77db7816067a4ec1
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-21 23:12:33 +0530

    Hardcoded_user added succeessfully

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
 
+const User = require('./models/user');
+
+app.post('/signup', async (req, res) => {
+  const userObj = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john_doe@gmail.com',
+    password: 'johndoe@123',
+  };
+  //Creating a new instance of the User Model
+  const user = new User(userObj);
+
+  await user.save();
+  res.send('User Added Successfully!');
+});
+
 connectDB()
   .then(() => {
     console.log('Database connection established...');
