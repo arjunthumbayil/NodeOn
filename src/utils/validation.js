71b6cb3bc3c19d9462de1164ff98dc7b3954d461
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-04-02 12:18:56 +0530

    Name validations inside helper validator functions added.

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,5 +5,7 @@ const validateSignupData = (req) => {
     throw new Error('Name is not valid!');
   } else if (firstName.length < 4 || firstName.length > 50) {
     throw new Error('FirstName should be 4-50 charaters');
+  } else if (lastName.length < 4 || lastName.length > 50) {
+    throw new Error('FirstName should be 4-50 charaters');
   }
 };
